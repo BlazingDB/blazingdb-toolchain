4401744981af4672b25fb92f5fd70a2857b3297d
Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
2019-03-19 16:12:29 -0500

    add script to setup the c/c++ compiler

---
+++ cmake/ConfigureHostCompiler.cmake
@@ -0,0 +1,28 @@
+#=============================================================================
+# Copyright 2019 BlazingDB, Inc.
+#     Copyright 2019 Percy Camilo Triveño Aucahuasi <percy@blazingdb.com>
+#=============================================================================
+
+# BEGIN macros
+
+# example:
+# configure_host_compiler(14, ON) for C++14 and force the standard is required
+macro(CONFIGURE_HOST_COMPILER HOST_CXX_STANDARD HOST_CXX_STANDARD_REQUIRED)
+    set(CMAKE_CXX_STANDARD ${HOST_CXX_STANDARD})
+    set(CMAKE_CXX_STANDARD_REQUIRED ${HOST_CXX_STANDARD_REQUIRED})
+    option(CXX_OLD_ABI "Use the old GLIBCXX11 ABI" ON)
+
+    message(STATUS "Using C++ standard: c++${CMAKE_CXX_STANDARD}")
+    message(STATUS "C++ standard required: ${CMAKE_CXX_STANDARD_REQUIRED}")
+    message(STATUS "Using C++ old ABI: ${CXX_OLD_ABI}")
+
+    if(CMAKE_COMPILER_IS_GNUCXX)
+        if(CXX_OLD_ABI)
+            message(AUTHOR_WARNING "Using old C++ GLIBCXX11 ABI -D_GLIBCXX_USE_CXX11_ABI=0")
+            set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0")
+            set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D_GLIBCXX_USE_CXX11_ABI=0")
+        else()
+            message(STATUS "Using modern/current C++ GLIBCXX11 ABI")
+        endif()
+    endif()
+endmacro()
