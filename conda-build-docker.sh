5e2795c766e918b870a305fd06c8ab75bb54be7d
Mario Inga <mario21ic@gmail.com>
2019-11-07 13:09:34 -0500

    feat(conda) fix cache and pkgs directories

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -3,12 +3,22 @@
 # example: ./conda-build-docker.sh blazingsql-nightly,rapidsai-nightly blazingsql-nightly 123
 
 export WORKSPACE=$PWD
-mkdir -p $PWD/cache/
+CONDA_RC=$PWD/.condarc
+CONDA_PKGS=$PWD/conda_pkgs/
+CONDA_CACHE=$PWD/conda_cache/
+
+if [ ! -f "$CONDA_RC" ]; then
+    touch $CONDA_RC
+fi
+mkdir -p $CONDA_PKGS $CONDA_CACHE
 
 docker run --rm \
     -u $(id -u):$(id -g) \
     -e CONDA_BUILD=$1 -e CONDA_UPLOAD=$2 -e MY_UPLOAD_KEY=$3 \
+    -v $CONDA_RC:/.condarc \
+    -v $CONDA_PKGS:/opt/conda/pkgs/ \
+    -v $CONDA_CACHE:/.cache/ \
     -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} \
-    -v $PWD/cache/:/opt/conda/pkgs/ \
     gpuci/miniconda-cuda:10.0-devel-ubuntu16.04 \
     ./ci/cpu/build.sh
+
