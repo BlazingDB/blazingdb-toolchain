0ad218aee80e7443d0cf9deffadbc9d4a079f88e
Mario Inga <mario21ic@gmail.com>
2019-11-07 11:17:40 -0500

    feat(conda) docker: adding cache directory

--- conda-build-docker.sh
+++ conda-build-docker.sh
@@ -5,7 +5,9 @@
 export WORKSPACE=$PWD
 
 docker run --rm \
+    -u $(id -u):$(id -g) \
     -e CONDA_BUILD=$1 -e CONDA_UPLOAD=$2 -e MY_UPLOAD_KEY=$3 \
     -v ${WORKSPACE}:${WORKSPACE} -w ${WORKSPACE} \
+    -v $PWD/cache/:/opt/conda/pkgs/ \
     gpuci/miniconda-cuda:10.0-devel-ubuntu16.04 \
     ./ci/cpu/build.sh
